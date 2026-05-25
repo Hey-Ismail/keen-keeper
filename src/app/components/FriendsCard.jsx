38f1cf3e80cf3c01e945fb1a7cdbb849670f891a
Hey-Ismail <Heythereitsfahim@gmail.com>
2026-05-25 21:59:35 +0600

    fixed : made it responsive

--- src/app/components/FriendsCard.jsx
+++ src/app/components/FriendsCard.jsx
@@ -1,48 +1,57 @@
-import Image from "next/image";
+'use client'
 
-const FriendsCard = ({ friend }) => {
+import Image from "next/image";
+import Link from "next/link";
+
+const FriendsCard = ({ data, }) => {
 
     const statusClass =
-        friend.status === "on-track"
+        data.status === "on-track"
             ? "bg-[#244d3fFF] text-white"
-            : friend.status === "almost due"
+            : data.status === "almost due"
                 ? "bg-[#efad44FF] text-white"
                 : "bg-[#ef4444FF] text-white";
 
     return (
-        <div className="bg-white rounded-xl border border-gray-200 p-6 text-center shadow-sm hover:shadow-lg transition duration-300">
-            <div className="flex flex-col items-center">
-                <Image
-                    src={friend.picture}
-                    alt={friend.name}
-                    width={70}
-                    height={70}
-                    className="rounded-full border-2 border-gray-200"
-                />
+        //* link is wokring
+        <Link href={`/home/${data.id}`}>
 
-                <h3 className="font-semibold text-lg mt-4 text-gray-900">
-                    {friend.name}
-                </h3>
+            <div className="bg-white rounded-xl border border-gray-200 p-6 text-center shadow-sm hover:shadow-lg transition duration-300">
+                <div className="flex flex-col items-center">
+                    <Image
+                        src={data.picture}
+                        alt={data.name}
+                        width={70}
+                        height={70}
+                        className="rounded-full border-2 border-gray-200"
+                    />
 
-                <p className="text-xs text-gray-400 mt-1">
-                    {friend.days_since_contact}d ago
-                </p>
+                    <h3 className="font-semibold text-lg mt-4 text-gray-900">
+                        {data.name}
+                    </h3>
 
-                <div className="flex flex-wrap justify-center gap-2 mt-3">
-                    {friend.tags.map((tag, index) => (
-                        <span
-                            key={index}
-                            className="px-2 py-1 text-xs rounded bg-green-100 text-green-700">
-                            {tag}
-                        </span>
-                    ))}
+                    <p className="text-xs text-gray-400 mt-1">
+                        {data.days_since_contact}d ago
+                    </p>
+
+                    <div className="flex flex-wrap justify-center gap-2 mt-3">
+                        {data.tags.map((tag, index) => (
+                            <span
+                                key={index}
+                                className="px-2 py-1 text-xs rounded bg-green-100 text-green-700">
+                                {tag}
+                            </span>
+                        ))}
+                    </div>
+
+                    <span className={`mt-4 px-3 py-1 rounded-full text-xs font-medium ${statusClass}`}>
+                        {data.status}
+                    </span>
                 </div>
-
-                <span className={`mt-4 px-3 py-1 rounded-full text-xs font-medium ${statusClass}`}>
-                    {friend.status}
-                </span>
             </div>
-        </div>
+        </Link>
+
+
     );
 };
 
